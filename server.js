7b61bbe80b58eaf8b4bfacdee55fb1a7629c42dd
Sergey <dubich.sv@gmail.com>
2016-11-29 18:06:08 +0200

    new server

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 
 // Create our app
 var app = express();
+
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-    if (req.header['x-forwarder-proto'] === 'http') {
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'http') {
         next();
     } else {
         res.redirect('http://' + req.hostname + req.url);
@@ -16,4 +17,4 @@ app.use(express.static('public'));
 
 app.listen(PORT, function () {
     console.log('Express server is up on port ' + PORT);
-})
+});
